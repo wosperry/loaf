a0f1b083bd5a8fda04b78481dcc511ebcb5aa6d3
wosperry <593281239@qq.com>
2022-11-04 00:12:12 +0800

    Modularity

---
+++ Loaf.Core/Modularity/DependsOnAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Loaf.Core.Modularity;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class DependsOnAttribute : Attribute
+{
+    /// <summary>
+    /// 依赖的模块
+    /// </summary>
+    public Type[] ModuleTypes { get; }
+    public DependsOnAttribute(params Type[] moduleTypes)
+    {
+        ModuleTypes = moduleTypes;
+    }
+}
---
+++ Loaf.Core/Modularity/LoafModule.cs
@@ -0,0 +1,16 @@
+﻿namespace Loaf.Core.Modularity;
+
+public abstract class LoafModule
+{
+    public virtual void PreConfigureService(ServiceConfigurationContext context)
+    {
+    }
+
+    public virtual void ConfigureService(ServiceConfigurationContext context)
+    {
+    }
+
+    public virtual void PostConfigureService(ServiceConfigurationContext context)
+    {
+    }
+}
