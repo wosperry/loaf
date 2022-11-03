61dacad23b240877a5f3639f082f794714be0267
wosperry <593281239@qq.com>
2022-11-04 00:12:03 +0800

    Modulality

---
+++ WebApplication1/MyModule.cs
@@ -0,0 +1,25 @@
+﻿using Loaf.Core.Modularity;
+
+[DependsOn(
+    typeof(MyBaseModule1),
+    typeof(MyBaseModule2)
+)]
+public class MyModule : LoafModule
+{
+    public override void ConfigureService(ServiceConfigurationContext context)
+    {
+        Console.WriteLine("ִ�е���MyModule ConfigureService");
+        var services = context.Services;
+        services.AddSwaggerDocument();
+        services.AddControllers();
+    }
+
+    public override void PreConfigureService(ServiceConfigurationContext context)
+    {
+        Console.WriteLine("ִ�е���MyModule PreConfigureService");
+    }
+    public override void PostConfigureService(ServiceConfigurationContext context)
+    {
+        Console.WriteLine("ִ�е���MyModule PostConfigureService");
+    }
+}
