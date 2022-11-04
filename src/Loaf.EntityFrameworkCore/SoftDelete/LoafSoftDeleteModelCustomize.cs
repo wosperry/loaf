aa1afa3d62e7389a27c8d12d70ad88c6c6953aa5
wosperry <admin@wosperry.com>
2022-11-04 15:51:58 +0800

    默认发现DbContext所在程序集的IEntityTypeCofniguration

--- src/Loaf.EntityFrameworkCore/SoftDelete/LoafSoftDeleteModelCustomize.cs
+++ src/Loaf.EntityFrameworkCore/SoftDelete/LoafSoftDeleteModelCustomize.cs
@@ -1,9 +1,8 @@
 using System.Linq.Expressions;
-using Loaf.EntityFrameworkCore.SoftDelete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
-namespace Loaf.EntityFrameworkCore;
+namespace Loaf.EntityFrameworkCore.SoftDelete;
 
 public class LoafModelCustomize : ModelCustomizer
 {
@@ -14,19 +13,22 @@ public class LoafModelCustomize : ModelCustomizer
     public override void Customize(ModelBuilder modelBuilder, DbContext context)
     {
         base.Customize(modelBuilder, context);
+        
+        // 默认发现DbContext所在程序集的所有IEntityTypeConfiguration
+        modelBuilder.ApplyConfigurationsFromAssembly(context.GetType().Assembly);
+        
+        // 干预查询过程，如果标记ISoftDelete，则不返回结果
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
-            if (typeof(ISoftDelete).IsAssignableFrom(entityType.ClrType))
-            {
-                var parameterExpression = Expression.Parameter(entityType.ClrType, "t");
-
-                var lambdaExpression = Expression.Lambda(
-                    Expression.Equal(
-                        Expression.Property(parameterExpression, nameof(ISoftDelete.IsDeleted)),
-                        Expression.Constant(false)
-                    ), parameterExpression);
-                entityType.SetQueryFilter(lambdaExpression);
-            }
+            if (!typeof(ISoftDelete).IsAssignableFrom(entityType.ClrType)) continue;
+            
+            var parameterExpression = Expression.Parameter(entityType.ClrType, "t");
+            var lambdaExpression = Expression.Lambda(
+                Expression.Equal(
+                    Expression.Property(parameterExpression, nameof(ISoftDelete.IsDeleted)),
+                    Expression.Constant(false)
+                ), parameterExpression);
+            entityType.SetQueryFilter(lambdaExpression);
         }
     }
 }
