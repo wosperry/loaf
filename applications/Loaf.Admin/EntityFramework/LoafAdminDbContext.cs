f678f2b660f2d9f5e1bd817696fbf05bce96dcbd
wosperry <admin@wosperry.com>
2022-11-04 15:51:15 +0800

    默认context构造函数参数改为携带当前类型泛型的DbContextOptions

---
+++ applications/Loaf.Admin/EntityFramework/LoafAdminDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Loaf.EntityFrameworkCore.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loaf.Admin.EntityFramework;
+
+public class LoafAdminDbContext:LoafDbContext
+{
+    public LoafAdminDbContext(DbContextOptions<LoafAdminDbContext> options) : base(options)
+    {
+    }
+}
