a9e7258c3ce515e58a74ef46a428be77d69a785b
wosperry <55421194+wosperry@users.noreply.github.com>
2022-11-10 17:10:43 +0800

    fix: edit drone yml

--- applications/Loaf.Admin/Services/Users/Dtos/UserCreateDto.cs
+++ applications/Loaf.Admin/Services/Users/Dtos/UserCreateDto.cs
@@ -1,4 +1,5 @@
-﻿namespace Loaf.Admin.Services.Users.Dtos
+﻿#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+namespace Loaf.Admin.Services.Users.Dtos
 {
     public class UserCreateDto
     {
